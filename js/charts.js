3de6e4baaf357323863fb1d2684c97755a633cd2
osvaldoferraz <osvaldo.ferraz@gmail.com>
2021-01-22 13:51:29 -0600

    Deliverable 1 done

--- js/charts.js
+++ js/charts.js
@@ -58,31 +58,46 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples/samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-    var metadata = data.metadata
-    console.log(metadata)
-    // 4. Create a variable that filters the samples for the object with the desired sample number.
+    var sampleData = data.samples
+  
+    // // 4. Create a variable that filters the samples for the object with the desired sample number.
+    var resultSample = sampleData.filter(sampleObj => sampleObj.id == sample)
+  
+    // // 5. Create a variable that holds the first sample in the array.
+    var result = resultSample[0]
+   
 
-    // 5. Create a variable that holds the first sample in the array.
-
-
-    // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    // // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+    var otuId = result.otu_ids
+    var otuLabels = result.otu_labels
+    var sampleValues = result.sample_values
+  
 
     // 7. Create the yticks for the bar chart.
+  
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = 
+    var yticks = otuId.slice(0,10).reverse().map(value => `OTU ${value} `)
+    var topValues = sampleValues.slice(0,10).reverse()
+    var hoverLabels = otuLabels.slice(0,10).reverse()
+    
+
 
     // // 8. Create the trace for the bar chart. 
-    // var barData = [
-      
-    // ];
+    var barData = [{
+      x : topValues,
+      y : yticks,
+      text: hoverLabels,
+      type : "bar",
+      orientation: 'h' 
+    }];
     // // 9. Create the layout for the bar chart. 
-    // var barLayout = {
-     
-    // };
+    var barLayout = {
+      title: "Top 10 Bacteria Cultures Found"
+    };
     // // 10. Use Plotly to plot the data with the layout. 
-    
+    Plotly.newPlot('bar', barData, barLayout)
   });
+  
 }
