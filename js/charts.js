16f314f332ec2fa7c699b5429b8bc484de235777
osvaldoferraz <osvaldo.ferraz@gmail.com>
2021-01-22 11:38:29 -0600

    samples folder

--- js/charts.js
+++ js/charts.js
@@ -3,7 +3,7 @@ function init() {
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("samples.json").then((data) => {
+  d3.json("samples/samples.json").then((data) => {
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -53,35 +53,36 @@ function buildMetadata(sample) {
   });
 }
 
-// // 1. Create the buildCharts function.
-// function buildCharts(sample) {
-//   // 2. Use d3.json to load and retrieve the samples.json file 
-//   d3.json("samples.json").then((data) => {
-//     // 3. Create a variable that holds the samples array. 
+// 1. Create the buildCharts function.
+function buildCharts(sample) {
+  // 2. Use d3.json to load and retrieve the samples.json file 
+  d3.json("samples.json").then((data) => {
+    // 3. Create a variable that holds the samples array. 
+    var metadata = data.metadata
+    console.log(metadata)
+    // 4. Create a variable that filters the samples for the object with the desired sample number.
 
-//     // 4. Create a variable that filters the samples for the object with the desired sample number.
-
-//     //  5. Create a variable that holds the first sample in the array.
-
-
-//     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+    // 5. Create a variable that holds the first sample in the array.
 
 
-//     // 7. Create the yticks for the bar chart.
-//     // Hint: Get the the top 10 otu_ids and map them in descending order  
-//     //  so the otu_ids with the most bacteria are last. 
+    // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
 
-//     var yticks = 
 
-//     // 8. Create the trace for the bar chart. 
-//     var barData = [
+    // 7. Create the yticks for the bar chart.
+    // Hint: Get the the top 10 otu_ids and map them in descending order  
+    //  so the otu_ids with the most bacteria are last. 
+
+    var yticks = 
+
+    // 8. Create the trace for the bar chart. 
+    var barData = [
       
-//     ];
-//     // 9. Create the layout for the bar chart. 
-//     var barLayout = {
+    ];
+    // 9. Create the layout for the bar chart. 
+    var barLayout = {
      
-//     };
-//     // 10. Use Plotly to plot the data with the layout. 
+    };
+    // 10. Use Plotly to plot the data with the layout. 
     
-//   });
-// }
+  });
+}
