b810cdab6ff36538ae61ceba4ba0c3f7eb764ccd
osvaldoferraz <osvaldo.ferraz@gmail.com>
2021-01-22 11:42:38 -0600

    charts

--- js/charts.js
+++ js/charts.js
@@ -32,7 +32,7 @@ function optionChanged(newSample) {
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("samples.json").then((data) => {
+  d3.json("samples/samples.json").then((data) => {
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
@@ -56,7 +56,7 @@ function buildMetadata(sample) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("samples.json").then((data) => {
+  d3.json("samples/samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
     var metadata = data.metadata
     console.log(metadata)
@@ -74,15 +74,15 @@ function buildCharts(sample) {
 
     var yticks = 
 
-    // 8. Create the trace for the bar chart. 
-    var barData = [
+    // // 8. Create the trace for the bar chart. 
+    // var barData = [
       
-    ];
-    // 9. Create the layout for the bar chart. 
-    var barLayout = {
+    // ];
+    // // 9. Create the layout for the bar chart. 
+    // var barLayout = {
      
-    };
-    // 10. Use Plotly to plot the data with the layout. 
+    // };
+    // // 10. Use Plotly to plot the data with the layout. 
     
   });
 }
